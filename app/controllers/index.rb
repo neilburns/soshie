62c30aa5e4320d1af069ad6ea9810e6f4342b252
Sherwood Callaway <shcallaway@Sherwoods-MacBook-Pro.local>
2014-07-06 19:33:14 -0700

    Implemented full geolocation feature.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,11 @@
 get '/' do
-  @events = Event.all
-  # latitude = params[:latitude].to_f
-  # longitude = params[:longitude].to_f
-  # p longitude
-  # p latitude
-  # @events = Event.near([latitude, longitude], 10)
-  # @events = Event.near([37.7843511, -122.3972421], 10)
   erb :index
+end
+
+get '/location' do
+  latitude = params[:latitude].to_f
+  longitude = params[:longitude].to_f
+  Event.near([latitude, longitude], 10).to_json
 end
 
 post '/new_event' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,8 @@
-
-// controls event_info opening and closing
-// $('.event').on('mouseover', function(){
-//   $(this).find('.event_info').slideDown();
-// });
-// $('.event').on('mouseleave', function(){
-//   $(this).find('.event_info').slideUp();
-// });
-
+// on window load, run these methods
+// that manipulate the built-in HTML5
+// navigator.
 window.onload = function() {
-  // grabs location of user device
+  // grabs coordinates of user device
   function getLocation() {
       if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(showPosition);
@@ -16,30 +10,57 @@ window.onload = function() {
           alert("Geolocation is not supported by this browser.")
       }
   }
-  // sends location to index
+
+  // sends coordinates to index route
   function showPosition(position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      $.ajax({
-        url: '/',
-        type: 'get',
-        data: {latitude: latitude, longitude: longitude},
-      });
+    $.ajax({
+      url: '/location',
+      type: 'get',
+      data: {latitude: position.coords.latitude, longitude: position.coords.longitude},
+      success: function(response) {
+        // append the retrieved events
+        writePanels($.parseJSON(response));
+      }
+    });
   }
+  // begins method chain
   getLocation();
 }
 
-// controls sliding event info panels
-$('.single_event').on('click', function(){
+// appends "single_events" to the DOM
+function writePanels(data) {
+  // grab the event panel template from the DOM
+  var template = $.trim($('.panel_template').html());
+  for (var i = 0; i < data.length; i++) {
+    // grab the particular segment of the JSON
+    // response that represents a single event
+    // and stores it as an object.
+    var event_object = data[i];
+    // create a new event panel from the template
+    var $new_panel = $(template);
+    // set the parts of the panel
+    // $new_panel.find('.single_event').show();
+    $new_panel.find(".image_panel").css("background-image", "url(" + event_object.imgurl + ")");
+    $new_panel.find(".event_title").text(event_object.title);
+    $new_panel.find(".event_date").text(event_object.date);
+    $new_panel.find(".info_panel_text_container").text(event_object.description);
+    // add the newly created panel to the DOM
+    $("#event_feed_container").append($new_panel);
+  }
+}
+
+// controls sliding event info panels.
+// must be added as a document listener,
+// because "single_event" elements are
+// being appened to the DOM after load.
+$(document).on('click','.single_event',function(){
+  // slide up all panels and containers that
+  // are NOT associated with the event target.
   $('.info_panel').not($(this).find('.info_panel')).slideUp();
   $('.info_panel_text_container').not($(this).find('.info_panel_text_container')).slideUp();
+  // slide down the panel and container
+  // associated with the event target.
   $(this).find('.info_panel').show();
   $(this).find('.info_panel_text_container').slideToggle();
 });
 
-// opens new event form
-$('#create_button').on('click', function(){
-  $('.new_event_form').show();
-  $('#create_button').hide();
-  $('#discover_button').hide();
-});
