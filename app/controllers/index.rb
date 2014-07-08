a7e92993bacbdf60b383d08436d561f73df0fc39
Sherwood Callaway <shcallaway@Sherwoods-MacBook-Pro.local>
2014-07-08 15:51:28 -0700

    Added more data to the seed file.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,7 +5,7 @@ end
 get '/location' do
   latitude = params[:latitude].to_f
   longitude = params[:longitude].to_f
-  Event.near([latitude, longitude], 10).to_json
+  Event.near([latitude, longitude], 50).to_json
 end
 
 post '/new_event' do
